e97564047c9496ffb4d2943de3324b82a70833de
senaygui <senaygeletaw@gmail.com>
2017-02-19 17:22:10 +0300

    generate model,view and migrated devise, add an admin signin inlink to the nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
-  root 'static_pages#home'
+  devise_for :admins
+  
 
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
@@ -8,6 +9,6 @@ Rails.application.routes.draw do
   get 'help' => 'static_pages#help'
   get 'feedback' => 'static_pages#feedback'
   get 'fqa' =>'static_pages#fqa'
-
+  root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
